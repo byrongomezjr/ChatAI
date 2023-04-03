219cd4ab514251cb2e6233e5782466334cd94a07
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-04-03 13:18:11 -0400

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Byron Gomez Jr on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
